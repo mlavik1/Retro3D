3703de54156a416e9fd2002cebb8d605a03d1cb1
Matias Lavik <matias.lavik@hotmail.com>
2019-04-11 21:23:06 +0200

    Removed the "Resources" folder from the project base directory and created a new "Basic" sample project.

--- emcc_generate.py
+++ emcc_generate.py
@@ -1,5 +1,9 @@
 from __future__ import print_function
-import glob, os
+import glob, os, sys
+
+projdir = "projects/samples/basic"
+if len(sys.argv) > 1:
+	projdir = sys.argv[1]
 
 f = open('emcc_build.bat', 'w')
 f.write('mkdir -p webbuild\n')
@@ -29,7 +33,7 @@ for file in glob.glob("**/**/**/**/*.cpp"):
     f.write('Retro3D/src/' + file + " ")
 
 # ALLOW_MEMORY_GROWTH=0 seems to cause a crash with SDL & TTF
-f.write('-I include -I Retro3D/src -s WASM=1 -s DISABLE_EXCEPTION_CATCHING=0 -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s USE_SDL_TTF=2 -s ALLOW_MEMORY_GROWTH=0 -s TOTAL_MEMORY=167772160 --use-preload-plugins --preload-file resources -o webbuild/main.html --std=c++14 -DCHAISCRIPT_NO_THREADS -DCHAISCRIPT_NO_THREADS_WARNING')
+f.write('-I include -I Retro3D/src -s WASM=1 -s DISABLE_EXCEPTION_CATCHING=0 -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s USE_SDL_TTF=2 -s ALLOW_MEMORY_GROWTH=0 -s TOTAL_MEMORY=167772160 --use-preload-plugins --preload-file ' + projdir + '/resources -o webbuild/main.html --std=c++14 -DCHAISCRIPT_NO_THREADS -DCHAISCRIPT_NO_THREADS_WARNING')
 
 # Debug parameters
 #f.write('-s ASSERTIONS=1 -s SAFE_HEAP=1 -s SAFE_HEAP_LOG=1 -s STACK_OVERFLOW_CHECK=2')
